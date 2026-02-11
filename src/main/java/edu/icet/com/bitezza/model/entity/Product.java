78d1b5e95a6be13aef20bbbee6beec8ab7df716a
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-02-11 16:57:49 +0530

    Add Product Entity for product data transfer

--- src/main/java/edu/icet/com/bitezza/model/entity/Product.java
+++ src/main/java/edu/icet/com/bitezza/model/entity/Product.java
@@ -1,6 +1,21 @@
 package edu.icet.com.bitezza.model.entity;
 
+import lombok.*;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Product {
 
+    private String id;
+    private String name;
+    private String description;
+    private double price;
+    private String imageUrl;
+    private boolean active;
+    private String categoryName;
 
 }
