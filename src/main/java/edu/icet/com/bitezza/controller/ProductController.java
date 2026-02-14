b344d941ad8c09eea344703840b92c63a08ffdcc
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-02-14 20:53:58 +0530

    Add getProduct endpoint to fetch product by name

--- src/main/java/edu/icet/com/bitezza/controller/ProductController.java
+++ src/main/java/edu/icet/com/bitezza/controller/ProductController.java
@@ -4,14 +4,13 @@ package edu.icet.com.bitezza.controller;
 import edu.icet.com.bitezza.model.dto.ProductDTO;
 import edu.icet.com.bitezza.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/product")
+@CrossOrigin(origins = "http://localhost:4200")
 public class ProductController {
 
     @Autowired
@@ -24,9 +23,12 @@ public class ProductController {
 
     @GetMapping("/getAll")
     public List<ProductDTO> getAllProducts(){
-
         return productService.getAllProducts();
+    }
 
+    @GetMapping("/getProduct")
+    public ProductDTO getProduct(@RequestParam String name){
+        return productService.getProduct(name);
     }
 
 }
