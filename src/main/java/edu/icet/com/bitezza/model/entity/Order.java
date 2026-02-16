10d8903b6086157725a354c3f9554d0dff331790
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-02-16 12:55:16 +0530

    Create Order entity with one-to-many relationship to OrderItem

--- src/main/java/edu/icet/com/bitezza/model/entity/Order.java
+++ src/main/java/edu/icet/com/bitezza/model/entity/Order.java
@@ -1,7 +1,27 @@
 package edu.icet.com.bitezza.model.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
 public class Order {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderId;
 
+    private String orderStatus;
+
+    private BigDecimal totalValue;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<OrderItem> items;
 
 }
