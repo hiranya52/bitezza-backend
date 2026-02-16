8388efd0e3dc667fa421d23241bfb9466707c04a
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-02-16 15:37:16 +0530

    Add OrderDTO with order items list

--- src/main/java/edu/icet/com/bitezza/model/dto/OrderDTO.java
+++ src/main/java/edu/icet/com/bitezza/model/dto/OrderDTO.java
@@ -1,4 +1,19 @@
 package edu.icet.com.bitezza.model.dto;
 
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class OrderDTO {
+
+    private Long orderId;
+    private String orderStatus;
+    private BigDecimal totalValue;
+    private List<OrderItemDTO> items;
+
 }
