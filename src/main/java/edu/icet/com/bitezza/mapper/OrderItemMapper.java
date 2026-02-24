2a06789338ed18af62cbe37483ec4c7232b6e4a6
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-02-24 20:58:30 +0530

    Update order item mapper class

--- src/main/java/edu/icet/com/bitezza/mapper/OrderItemMapper.java
+++ src/main/java/edu/icet/com/bitezza/mapper/OrderItemMapper.java
@@ -3,28 +3,33 @@ package edu.icet.com.bitezza.mapper;
 import edu.icet.com.bitezza.model.dto.OrderItemDTO;
 import edu.icet.com.bitezza.model.entity.Order;
 import edu.icet.com.bitezza.model.entity.OrderItem;
+import edu.icet.com.bitezza.model.entity.Product;
 
 public class OrderItemMapper {
 
     public static OrderItem toEntity(OrderItemDTO dto, Order order) {
-        if (dto == null) return null;
 
         OrderItem item = new OrderItem();
-        item.setProductId(dto.getProductId());
+
         item.setQuantity(dto.getQuantity());
         item.setPrice(dto.getPrice());
         item.setOrder(order);
 
+        Product product = new Product();
+        product.setId(dto.getProductId());   // BIT-100
+
+        item.setProduct(product);
+
         return item;
     }
 
-    public static OrderItemDTO toDTO(OrderItem entity) {
-        if (entity == null) return null;
+    public static OrderItemDTO toDTO(OrderItem item) {
 
         OrderItemDTO dto = new OrderItemDTO();
-        dto.setProductId(entity.getProductId());
-        dto.setQuantity(entity.getQuantity());
-        dto.setPrice(entity.getPrice());
+
+        dto.setProductId(item.getProduct().getId());
+        dto.setQuantity(item.getQuantity());
+        dto.setPrice(item.getPrice());
 
         return dto;
     }
