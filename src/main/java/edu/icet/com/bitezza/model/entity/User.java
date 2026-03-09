489a263b65c3bb7bcf011f0919eba741ae484288
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-03-09 16:35:37 +0530

    Add User Entity class

--- src/main/java/edu/icet/com/bitezza/model/entity/User.java
+++ src/main/java/edu/icet/com/bitezza/model/entity/User.java
@@ -1,4 +1,15 @@
 package edu.icet.com.bitezza.model.entity;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import org.springframework.data.annotation.Id;
+
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String username;
+    private String password;
+
 }
