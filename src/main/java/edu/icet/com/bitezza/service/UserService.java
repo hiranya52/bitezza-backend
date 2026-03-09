3607c611be855b93396ef6edcd2184a3ec0ad608
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-03-09 16:57:00 +0530

    Implement getUser method to fetch user by username and map to UserDTO

--- src/main/java/edu/icet/com/bitezza/service/UserService.java
+++ src/main/java/edu/icet/com/bitezza/service/UserService.java
@@ -2,6 +2,7 @@ package edu.icet.com.bitezza.service;
 
 import edu.icet.com.bitezza.model.dto.UserDTO;
 import edu.icet.com.bitezza.repository.UserRepository;
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class UserService {
@@ -11,7 +12,12 @@ public class UserService {
 
     public UserDTO getUser(String username) {
 
+        User user = userRepository.findByUsername(username);
 
+        return new UserDTO(
+                user.getUsername(),
+                user.getPassword()
+        );
 
     }
 }
