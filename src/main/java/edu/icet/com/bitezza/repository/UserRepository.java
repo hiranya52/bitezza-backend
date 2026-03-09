57d6859d1e644e3472c430a73fe6ee7efe82fd43
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-03-09 16:52:38 +0530

    Add findByUsername method to fetch user by username

--- src/main/java/edu/icet/com/bitezza/repository/UserRepository.java
+++ src/main/java/edu/icet/com/bitezza/repository/UserRepository.java
@@ -4,4 +4,7 @@ import org.apache.catalina.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer>{
+
+    User findByUsername(String username);
+
 }
