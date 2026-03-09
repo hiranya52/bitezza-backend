2d0b02ac3c959add07d24eccf633369a8661a43b
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-03-09 22:24:57 +0530

    Add @PathVariable to getUser endpoint to retrieve user by username

--- src/main/java/edu/icet/com/bitezza/controller/UserController.java
+++ src/main/java/edu/icet/com/bitezza/controller/UserController.java
@@ -3,10 +3,7 @@ package edu.icet.com.bitezza.controller;
 import edu.icet.com.bitezza.model.dto.UserDTO;
 import edu.icet.com.bitezza.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
@@ -17,7 +14,7 @@ public class UserController {
     UserService userService;
 
     @GetMapping("/getUser")
-    public UserDTO getUser(String username){
+    public UserDTO getUser(@PathVariable String username){
         return userService.getUser(username);
     }
 
